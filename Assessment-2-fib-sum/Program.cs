b0529d95da3e7423ac05c424ea9392da44404ebf
kabelwamanong <kgotsoyaso@gmail.com>
2020-01-03 08:18:58 +0200

    Assessment 2

---
+++ Assessment-2-fib-sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assessment_2_fib_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Assessment 2: Finding the sum of even-valued terms in a Fibonacci sequence where terms dont exceed 5 million
+            var prev = 0;
+            var cur = 1;
+            var total = 0;
+
+            while (true)
+            {
+                var nx = prev + cur;
+                prev = cur;
+                cur = nx;
+
+                if (cur >= 5000000)
+                {
+                    break;
+                }
+                if (cur % 2 == 0)
+                {
+                    total += cur;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
